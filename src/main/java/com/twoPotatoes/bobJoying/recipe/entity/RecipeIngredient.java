9d5bde25515698e36a3032024b1ba526e955e59e
JisooPyo <audwl326@gmail.com>
2024-02-16 16:23:29 +0900

    init: recipe 도메인 모델 recipe, recipe_ingredient, recipe_link 구축

---
+++ src/main/java/com/twoPotatoes/bobJoying/recipe/entity/RecipeIngredient.java
@@ -0,0 +1,46 @@
+package com.twoPotatoes.bobJoying.recipe.entity;
+
+import com.twoPotatoes.bobJoying.ingredient.entity.Ingredient;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeIngredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipeId")
+    private Recipe recipe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredientId")
+    private Ingredient ingredient;
+
+    @Column(nullable = false)
+    private Float quantity;
+
+    @Column(length = 10, nullable = false)
+    private String unit;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isMainIngredient = true;
+
+}
