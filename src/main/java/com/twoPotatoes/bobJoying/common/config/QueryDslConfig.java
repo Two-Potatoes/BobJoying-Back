5cbc8ddd791c8b67bf561653f741d0ef0301a63b
JisooPyo <audwl326@gmail.com>
2024-02-28 17:41:49 +0900

    [init] 프로젝트 기초 작업 (#19)
    * build: QueryDsl 의존성 추가
    
    * build: QueryDsl 빌드 옵션 설정
    
    * docs: .gitignore에 QueryDsl 파일 경로 추가
    
    * feat: QueryDslConfig 설정
    
    * chore: QueryDslConfig 설명 주석 추가
    
    * feat: 컨트롤러에서 발생하는 예외 처리

---
+++ src/main/java/com/twoPotatoes/bobJoying/common/config/QueryDslConfig.java
@@ -0,0 +1,23 @@
+package com.twoPotatoes.bobJoying.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+// JPAQueryFactory를 사용하기 위해 Bean으로 등록
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
---
+++ src/main/java/com/twoPotatoes/bobJoying/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.twoPotatoes.bobJoying.common.exception;
+
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import graphql.GraphQLError;
+
+// 컨트롤러에서 예외 발생시 여기서 핸들링하여 errorCode와 메시지를 리턴합니다.
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // CustomException 발생 시 작동하는 핸들러
+    @GraphQlExceptionHandler
+    public GraphQLError handle(CustomException ex) {
+        return GraphQLError.newError()
+            .errorType(ex.getErrorCode().getErrorType())
+            .message(ex.getMessage())
+            .build();
+    }
+}
